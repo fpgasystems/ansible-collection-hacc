5f304e33f48132113c6eae960b080545be50d05a
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2025-08-13 11:05:35 +0200

    Tools: [WIP] migrating to list of objects for releases

---
+++ filter_plugins/amd_apm_filters.py
@@ -0,0 +1,31 @@
+from ansible.errors import AnsibleFilterError
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'normalize_releases_list': self.normalize_releases_list
+        }
+
+    def normalize_releases_list(self, item):
+        # Filter to fill in all defaults of the amd_apm_releases list, when not provided
+        #   When only a String is given, this is interpreted as the release string
+        #   Otherwise a Dictionary is expected, with at least the release attribute. All missing attributes are filled in with defaults
+        if isinstance(item, str):
+            return {
+                'release': item,
+                'install_method': 'zip',
+                'state': 'present',
+                'update_level': 'base'
+            }
+        elif isinstance(item, dict):
+            if 'release' not in item:
+                raise AnsibleFilterError("Missing required 'release' key in object")
+            return {
+                'release': item['release'],
+                'install_method': item.get('install_method', 'zip'),
+                'state': item.get('state', 'present'),
+                'update_level': item.get('update_level', 'base')
+            }
+        else:
+            raise AnsibleFilterError(f"Unsupported type: {type(item)}")
+
